3a533cb1905a15771a1df14c721e758e36bc60dc
veronica2024 <vn710949@gmail.com>
2024-08-02 10:15:08 -0300

    criado apiAtividade

--- server.js
+++ server.js
@@ -1,5 +1,21 @@
 // importa oas bibliotecas
-
 const express = require('express'); // 
-const dotenv = require('dotenv')
+const dotenv = require('dotenv');
+const cors = require('cors');
+const bodyParser = require('bodyParser');
+
+//configurar as variáveis de ambiente 
+dotenv.config(); // carregar as variáveis de ambiente definidas no arquivo .env para o process.env.
+
+
+//Inicializa uma nova aplicação Express 
+const app = express();
+
+
+//Configura o CORS e o body-parser 
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
 
