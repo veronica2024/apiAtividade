30446a1bdb8e946802cbd0f4e74419122d492a41
veronica2024 <vn710949@gmail.com>
2024-08-05 10:46:57 -0300

    apiAtividade

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express'); // 
 const dotenv = require('dotenv');
 const cors = require('cors');
-const bodyParser = require('bodyParser');
+const bodyParser = require('body-parser');
 
 //configurar as variáveis de ambiente 
 dotenv.config(); // carregar as variáveis de ambiente definidas no arquivo .env para o process.env.
@@ -17,5 +17,18 @@ app.use(cors());
 app.use(bodyParser.json());
 
 
+//Rota inicial para testar o servidor
+
+app.get('/',(req,res) => {
+    res.send('Servidor esta rodando')
+});
+
+// Configura o servidor para escutar em uma porta específica 
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => { 
+    console.log(`Servidor rodando na porta ${PORT}`); 
+   }); 
 
 
